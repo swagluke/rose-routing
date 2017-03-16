8cf2ff95ae3f312bf9dcbf0b0855d99e48eee0bb
Luke Zhang <zhangl@rose-hulman.edu>
2017-03-16 08:50:40 -0400

    Very basic routing via url works.

--- RoseRouting/src/app/app-routing.module.ts
+++ RoseRouting/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { HomeComponent } from './+home/home.component';
+import { SigninComponent } from './+signin/signin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: "home", pathMatch: "full" },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
--- RoseRouting/src/app/app.module.ts
+++ RoseRouting/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './+home/home.component';
+import { SigninComponent } from './+signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
